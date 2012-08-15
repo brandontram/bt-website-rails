8cd5d911f30ef2111ea5bede897747dcad7d72e0
Brandon Tram <Brandon@Brandons-MacBook-Pro.local>
2012-08-15 11:54:28 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BtWebsiteRails::Application.config.secret_token = '4af42ebf86ac686cb8f9c93d94c308868890a561f5ebd015870da7a7ebfd74ce56ecd5ef6d5ec8b80df5b61b9dce20fad11cdc83ecd7b4563ce894d93462391d'
